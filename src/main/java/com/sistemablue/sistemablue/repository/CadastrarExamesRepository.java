1ee14299eb7a6c5ee8fec58a255aaa390360630f
Jeferson Siqueira <jeferson.siqueira-terceiro@cea.com.br>
2023-10-26 03:16:43 -0300

    Refatoração do código para melhoria do projeto como um todo

--- src/main/java/com/sistemablue/sistemablue/repository/CadastrarExamesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sistemablue.sistemablue.repository;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface CadastrarExamesRepository extends ReactiveMongoRepository<AtendimentoExameDTO, ObjectId> {
-}
